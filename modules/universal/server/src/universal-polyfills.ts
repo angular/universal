12c688dd5452c91a15f49db43b649117855a6329
Wassim Chegham <maneki.nekko@gmail.com>
2016-01-25 20:31:32 +0100

    chore(server-patch): keep the import and rename the file to universal-polyfills.ts

--- modules/universal/server/src/universal-polyfills.ts
+++ modules/universal/server/src/universal-polyfills.ts
@@ -1,12 +1,12 @@
 
 // polyfills
-export * from 'es6-promise';
-export * from 'es6-shim';
+import from 'es6-promise';
+import from 'es6-shim';
 // typescript emit metadata
-export * from 'reflect-metadata';
+import from 'reflect-metadata';
 // zone.js to track promises
-export * from 'zone.js/dist/zone-microtask';
-export * from 'zone.js/dist/long-stack-trace-zone';
+import from 'zone.js/dist/zone-microtask';
+import from 'zone.js/dist/long-stack-trace-zone';
 
 // dom closure
 import {Parse5DomAdapter} from 'angular2/src/platform/server/parse5_adapter';
