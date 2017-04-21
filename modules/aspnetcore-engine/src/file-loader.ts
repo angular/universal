455e68adf0498c13d6e073f65a06b72ce12b0011
Mark Pieszak <mpieszak84@gmail.com>
2017-04-21 19:35:06 -0400

    separate out logic

---
+++ modules/aspnetcore-engine/src/file-loader.ts
@@ -0,0 +1,16 @@
+import { ResourceLoader } from '@angular/compiler';
+
+export class FileLoader implements ResourceLoader {
+  get(url: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      // install node types
+      fs.readFile(url, (err: NodeJS.ErrnoException, buffer: Buffer) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(buffer.toString());
+      });
+    });
+  }
+}
