0b43ae6e524a228900a375ea7fa02dac74f4cae1
Fabian Wiles <fabianwiles@live.com>
2018-02-25 10:58:52 +1300

    [squash] clean up

--- modules/aspnetcore-engine/public-api.ts
+++ modules/aspnetcore-engine/public-api.ts
@@ -8,8 +8,6 @@
 export { ngAspnetCoreEngine } from './src/main';
 export { createTransferScript } from './src/create-transfer-script';
 
-export { REQUEST, ORIGIN_URL } from './src/tokens';
-
 export { IEngineOptions } from './src/interfaces/engine-options';
 export { IRequestParams } from './src/interfaces/request-params';
 export { IEngineRenderResult } from './src/interfaces/engine-render-result';
