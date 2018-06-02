921ebd4172281bf7214ff38aafcf42e02acf0c00
Fabian Wiles <fabianwiles@live.com>
2018-06-03 10:08:32 +1200

    feat(common): TransferHttpCache now respects url params

--- modules/common/engine/private_api.ts
+++ modules/common/engine/private_api.ts
@@ -5,5 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
 export { FileLoader as ɵFileLoader, CommonEngine as ɵCommonEngine,
-   RenderOptions as ɵRenderOptions } from './src/index';
+  RenderOptions as ɵRenderOptions } from './src/index';
