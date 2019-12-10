f9d96eaec0bf61fe4e9aae310933e05997759874
vikerman <viks@google.com>
2019-12-10 13:47:18 -0800

    release: v8.2.3

--- tools/bazel-stamp-vars.sh
+++ tools/bazel-stamp-vars.sh
@@ -12,6 +12,16 @@ function onError {
 # Setup crash trap
 trap 'onError' ERR
 
+BUILD_SCM_VERSION_RAW=$(git describe --abbrev=7 --tags HEAD)
+if [[ $(git status --untracked-files=no --porcelain) ]]; then LOCAL_CHANGES="true"; else LOCAL_CHANGES="false"; fi
+
+
+# Reformat `git describe` version string into a more semver-ish string
+#   From:   5.2.0-rc.0-57-g757f886
+#   To:     5.2.0-rc.0+57.sha-757f886
+#   Or:     5.2.0-rc.0+57.sha-757f886.with-local-changes
+BUILD_SCM_VERSION="$(echo ${BUILD_SCM_VERSION_RAW} | sed -E 's/-([0-9]+)-g/+\1.sha-/g')""$( if [[ $LOCAL_CHANGES == "true" ]]; then echo ".with-local-changes"; fi)"
+echo BUILD_SCM_VERSION ${BUILD_SCM_VERSION}
 
 echo BUILD_SCM_HASH $(git rev-parse HEAD)
 
@@ -24,16 +34,7 @@ if [[ "$(git tag)" == "" ]]; then
   echo ""
 fi
 
-BUILD_SCM_VERSION_RAW=$(git describe --abbrev=7 --tags HEAD)
-
 # Find out if there are any uncommitted local changes
 # TODO(i): is it ok to use "--untracked-files=no" to ignore untracked files since they should not affect anything?
-if [[ $(git status --untracked-files=no --porcelain) ]]; then LOCAL_CHANGES="true"; else LOCAL_CHANGES="false"; fi
 echo BUILD_SCM_LOCAL_CHANGES ${LOCAL_CHANGES}
 
-# Reformat `git describe` version string into a more semver-ish string
-#   From:   5.2.0-rc.0-57-g757f886
-#   To:     5.2.0-rc.0+57.sha-757f886
-#   Or:     5.2.0-rc.0+57.sha-757f886.with-local-changes
-BUILD_SCM_VERSION="$(echo ${BUILD_SCM_VERSION_RAW} | sed -E 's/-([0-9]+)-g/+\1.sha-/g')""$( if [[ $LOCAL_CHANGES == "true" ]]; then echo ".with-local-changes"; fi)"
-echo BUILD_SCM_VERSION ${BUILD_SCM_VERSION}
