48d1e78d60061f8632dab40571c356c9aad1c019
Alan Agius <alan.agius4@gmail.com>
2020-04-13 11:02:29 +0200

    build: update types/express

--- modules/express-engine/tokens/injection-tokens.ts
+++ modules/express-engine/tokens/injection-tokens.ts
@@ -5,9 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
 import { InjectionToken } from '@angular/core';
 import { Request, Response } from 'express';
-import { ParamsDictionary } from 'express-serve-static-core';
 
-export const REQUEST = new InjectionToken<Request<ParamsDictionary>>('REQUEST');
+export const REQUEST = new InjectionToken<Request<Record<string, string>>>('REQUEST');
 export const RESPONSE = new InjectionToken<Response>('RESPONSE');
