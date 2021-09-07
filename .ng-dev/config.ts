ca39dffa32d9fccf791ccd3b0344a72db05f2a1f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-07 22:48:00 +0800

    Delete .ng-dev directory

--- .ng-dev/config.ts
+++
@@ -1,5 +0,0 @@
-export { commitMessage } from './commit-message';
-export { github } from './github';
-export { merge } from './merge';
-export { format } from './format';
-export { release } from './release';
