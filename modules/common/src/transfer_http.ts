891cbbd65ab2339b560622a4cdc1e1ae131b42dd
Alan Agius <alan.agius4@gmail.com>
2022-03-17 20:37:26 +0100

    build: update to TypeScript 4.6  (#2582)
    * build: update to TypeScript 4.6
    
    Closes #2544
    
    * build: update version to 13.1.0
    
    Bumps up the version to 13.1.0-rc.0 so the tooling can pick it up.

--- modules/common/src/transfer_http.ts
+++ modules/common/src/transfer_http.ts
@@ -56,7 +56,11 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
     );
   }
 
-  private makeCacheKey(method: string, url: string, params: HttpParams): StateKey<string> {
+  private makeCacheKey(
+    method: string,
+    url: string,
+    params: HttpParams,
+  ): StateKey<TransferHttpResponse> {
     // make the params encoded same as a url so it's easy to identify
     const encodedParams = params
       .keys()
@@ -65,7 +69,7 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
       .join('&');
     const key = (method === 'GET' ? 'G.' : 'H.') + url + '?' + encodedParams;
 
-    return makeStateKey<TransferHttpResponse>(key);
+    return makeStateKey(key);
   }
 
   constructor(appRef: ApplicationRef, private transferState: TransferState) {
@@ -97,7 +101,7 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
 
     if (this.transferState.hasKey(storeKey)) {
       // Request found in cache. Respond using it.
-      const response = this.transferState.get<TransferHttpResponse>(storeKey, {});
+      const response = this.transferState.get(storeKey, {});
 
       return observableOf(
         new HttpResponse<any>({
