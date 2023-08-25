0ba247220d202ffadf2b21cf144f08cb9fd8c8b4
alkavats1 <alka.vats@nashtechglobal.com>
2023-08-25 20:22:01 +0530

    docs: removed unused ngOnInit and constructor from lazy module

--- integration/clover/src/app/lazy/lazy.component.ts
+++ integration/clover/src/app/lazy/lazy.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lazy',
   templateUrl: './lazy.component.html',
   styleUrls: ['./lazy.component.css'],
 })
-export class LazyComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class LazyComponent {
+ 
 }
